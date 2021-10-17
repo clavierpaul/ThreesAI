d34997ef0dc81b5dbf92e2846a044f319cf03788
clavierpaul <53355311+clavierpaul@users.noreply.github.com>
2021-10-17 13:00:44 +0100

    Random board generation plus general refactoring and bug fixes

---
+++ src/Deck.fs
@@ -0,0 +1,23 @@
+﻿namespace ThreesAI
+
+open System
+
+type Deck = Tile list
+
+module Deck =
+    let private deck = [| Tile 1; Tile 1; Tile 1; Tile 1; Tile 2; Tile 2; Tile 2; Tile 2; Tile 3; Tile 3; Tile 3; Tile 3 |]
+    
+    let private random = Random ()
+    
+    // Fisher-Yates shuffle stolen and modified from Rosetta Code
+    // Returns a new shuffled deck
+    let create (): Deck =
+        let newDeck = Array.copy deck
+        let swap i j =
+            let item = newDeck.[i]
+            newDeck.[i] <- newDeck.[j]
+            newDeck.[j] <- item
+        let len = Array.length newDeck
+        [0 .. (len - 2)] |> List.iter (fun i -> swap i (random.Next(i, len)))
+        List.ofArray newDeck
+    
